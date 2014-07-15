f5e7f94efe391301faea5c03e0f598f782d17c0e
Gal Dubitski <gal@tocarta.es>
2014-07-15 18:21:41 +0100

    support featured events

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -2,11 +2,25 @@ class Api::V1::EventsController < Api::BaseController
     
     # Get Events
     def index
-      if !params[:category_ids].nil?
-        @events = Event.where(category_id: params[:category_ids], active: true).order('date ASC')
-      else
-        @events = Event.where(active: true).order('date ASC')
+      filters = { active: true }
+      
+      # get_events_by_categories
+      if params.has_key?(:category_ids)
+        filters[:category_id] = params[:category_ids]
       end
+      
+      # get_featured_events
+      if params.has_key?(:featured)
+        filters[:featured] = params[:featured]
+      end
+      
+      # get_events_by_location
+      if params.has_key?(:location) or params.has_key?(:city)
+        
+      end
+      
+      @events = Event.where(filters).order('date ASC')
+      
     end
     
 end
