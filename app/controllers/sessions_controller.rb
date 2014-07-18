a5b2013a5f26b736167e99ae20bec366b5843294
Gal Dubitski <gal@tocarta.es>
2014-07-18 10:57:29 +0100

    login accepts password or token

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,22 @@
 class SessionsController < Devise::RegistrationsController
   protect_from_forgery with: :null_session
-  acts_as_token_authentication_handler_for User
+  #acts_as_token_authentication_handler_for User
+  #before_action :authenticate_user!
   
   respond_to :json
   
   def new
-    if user_signed_in?
-      render :json => { :success => true }
+    verify_params
+    myuser = User.find_for_authentication({:email => params[:email]})
+    if myuser.valid_password? params[:password] or myuser.authentication_token==params[:auth_token]
+      render :json => { :success => true, :email => params[:email], :auth_token => myuser.authentication_token }
     else
       render :json => { :success => false }, status: :unauthorized
     end
+  end
+  
+  def verify_params
+    params.permit(:email,:password,:auth_token)
   end
   
 
