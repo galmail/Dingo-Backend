d3c9043f04a535f4ddea959dd49261e501a1a36b
Gal Dubitski <gal@tocarta.es>
2014-07-28 12:34:53 +0100

    ticket admin config

--- app/admin/ticket.rb
+++ app/admin/ticket.rb
@@ -1,8 +1,27 @@
 ActiveAdmin.register Ticket do
-  
+  config.filters = false
   permit_params :price, :seat_type, :description, :photo1, :photo2, :photo3, :event_id, :user_id, :delivery_options, :payment_options, :number_of_tickets, :face_value_per_ticket, :available
   
-
+  index do
+    column :id
+    column :price
+    column :seat_type
+    column :description
+    column :event_id
+    column :user_id
+    column :number_of_tickets
+    column :available
+    column :photo1 do |ad|
+      image_tag(ad.photo.url(:thumb))
+    end
+    column :photo2 do |ad|
+      image_tag(ad.photo.url(:thumb))
+    end
+    column :photo3 do |ad|
+      image_tag(ad.photo.url(:thumb))
+    end
+    actions
+  end
   
   # See permitted parameters documentation:
   # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
