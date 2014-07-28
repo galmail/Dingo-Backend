a9a892176876334fdfde99364c43899bd8959585
Gal Dubitski <gal@tocarta.es>
2014-07-28 13:49:13 +0100

    Accept/Reject Offer

--- app/controllers/api/v1/offers_controller.rb
+++ app/controllers/api/v1/offers_controller.rb
@@ -25,30 +25,59 @@ class Api::V1::OffersController < Api::BaseController
     @offers = Offer.where(filters).where(conditions).order('created_at DESC').limit(100)
   end
 
-  # Send Message
+  # Send Offer
   def create
     params.require(:receiver_id)
-    params.require(:content)
-    message_params = params.permit(:receiver_id, :content)
+    params.require(:ticket_id)
+    params.require(:num_tickets)
+    params.require(:price)
+    offer_params = params.permit(:receiver_id, :ticket_id, :num_tickets, :price)
 
-    message = Message.new(message_params)
-    message.sender = current_user
-    if message.save
-      send_message(message)
-      render :json=> message.as_json, status: :created
+    offer = Offer.new(offer_params)
+    offer.sender = current_user
+    if offer.save
+      send_offer(offer)
+      render :json=> offer.as_json, status: :created
     else
-      render :json=> message.errors, status: :unprocessable_entity
+      render :json=> offer.errors, status: :unprocessable_entity
     end
   end
+  
+  # Accept/Reject Offer
+  def update
+    params.require(:id)
+    params.require(:accept_offer)
+    offer_params = params.permit(:id, :accept_offer)
+    offer = Offer.find(params[:id])
+    offer.accepted = params[:accept_offer]
+    offer.rejected = !params[:accept_offer]
+    if offer.save
+      notify_offer(offer)
+      render :json=> offer.as_json, status: :updated
+    else
+      render :json=> offer.errors, status: :unprocessable_entity
+    end
+  end
+  
 
   private
 
-  # Sending message via push to device: https://github.com/NicosKaralis/pushmeup
-  def send_message(msg)
-    puts "Sending message: #{msg.content} to receiver_id #{msg.receiver_id}"
+  # Sending offer via push to device: https://github.com/NicosKaralis/pushmeup
+  def send_offer(offer)
+    puts "Sending offer_id: #{offer.id} to receiver_id #{offer.receiver_id}"
     
-    APNS.send_notifications(User.find(msg.receiver_id).devices.map { |device|
-      APNS::Notification.new(device.uid, msg.content)
+    APNS.send_notifications(User.find(offer.receiver_id).devices.map { |device|
+      APNS::Notification.new(device.uid, offer.price)
+      #APNS::Notification.new(device.uid, :alert => msg.content, :badge => 1, :sound => 'default')
+    })
+  end
+  
+  # Notify offer via push to device: https://github.com/NicosKaralis/pushmeup
+  def notify_offer(offer)
+    puts "Notify offer_id: #{offer.id} back to sender_id #{offer.sender_id}"
+    
+    APNS.send_notifications(User.find(offer.sender_id).devices.map { |device|
+      APNS::Notification.new(device.uid, offer.accepted)
       #APNS::Notification.new(device.uid, :alert => msg.content, :badge => 1, :sound => 'default')
     })
   end
