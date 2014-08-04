442ba45844331cdb4c21d564daa0e2082d6ef772
Gal Dubitski <gal@tocarta.es>
2014-08-04 21:35:46 +0100

    update user profile

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::UsersController < Api::BaseController
   def create
     puts "updating user..."
     user_data = params.permit(:name,:photo_url,:date_of_birth,:city)
-    current_user.update_all(user_data)
+    current_user.update_attributes(user_data)
     render :json => user.as_json
   end
   
