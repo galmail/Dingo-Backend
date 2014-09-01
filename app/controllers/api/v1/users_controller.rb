84b3e2e4a2269b23ef71be2b6985df4614e30c8a
Gal Dubitski <gal@tocarta.es>
2014-09-01 21:58:49 +0100

    added surname to update profile method

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::UsersController < Api::BaseController
   
   # Update User's Profile
   def create
-    user_data = params.permit(:name,:photo_url,:date_of_birth,:city)
+    user_data = params.permit(:name,:surname,:photo_url,:date_of_birth,:city)
     current_user.update_attributes(user_data)
     render :json => current_user.as_json
   end
