2ff5c13ea17945a7c1f7559026bcac9ea7ad97ae
Gal Dubitski <gal@tocarta.es>
2015-01-30 15:54:56 +0000

    promo API

--- app/controllers/api/v1/promos_controller.rb
+++ app/controllers/api/v1/promos_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::PromosController < Api::BaseController
       discount = 0
       promos = Promo.where(:code => current_user.promo, :active => true)
       if promos.length>0 and !current_user.promo_used
-        discount = promos.first.calculate_discount(params[:amount])
+        discount = promos.first.calculate_discount(params[:amount].to_f)
       end
       render :json => { :discount => discount }
     end
