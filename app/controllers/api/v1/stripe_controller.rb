7de9d8936647fc0f8f5c45d7e8a390d90c5e4bc7
Gal Dubitski <gal@tocarta.es>
2015-02-26 12:43:38 +0000

    handling stripe errors

--- app/controllers/api/v1/stripe_controller.rb
+++ app/controllers/api/v1/stripe_controller.rb
@@ -3,6 +3,7 @@ class Api::V1::StripeController < Api::BaseController
   def pay
     payment = params.permit(:token, :currency, :amount, :description)
     # Create the charge on Stripe's servers - this will charge the user's card
+    stripe_error = nil
     begin
       charge = Stripe::Charge.create(
         :amount => params[:amount], # amount in cents, again
@@ -10,11 +11,26 @@ class Api::V1::StripeController < Api::BaseController
         :source => params[:token],
         :description => params[:description]
       )
-      render :json=> payment.as_json, status: :ok
     rescue Stripe::CardError => e
-      # The card has been declined
-      render :json=> payment.as_json, status: :forbidden
+      stripe_error = e
+    rescue Stripe::InvalidRequestError => e
+      stripe_error = e
+    rescue Stripe::AuthenticationError => e
+      stripe_error = e
+    rescue Stripe::APIConnectionError => e
+      stripe_error = e
+    rescue Stripe::StripeError => e
+      stripe_error = e
     end
+    
+    if !stripe_error.nil?
+      body = stripe_error.json_body
+      err  = body[:error]
+      render :json=> err, status: stripe_error.http_status
+    else
+      render :json=> payment.as_json, status: :ok
+    end
+    
   end
   
 end
