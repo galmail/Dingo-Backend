a8e553cafb8c2ac8c47686a1b9636e09246592e9
Gal Dubitski <gal@tocarta.es>
2015-03-02 13:06:05 +0000

    updating gems

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module Dingoapp
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     config.time_zone = 'London'
     
-    config.serve_static_assets = true
+    config.serve_static_files = true
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
