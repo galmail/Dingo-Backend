c0f52a8ac9600188b995ec38b7a9101acb5ca543
Gal Dubitski <gal@tocarta.es>
2015-03-03 13:52:59 +0000

    device admin

--- app/admin/device.rb
+++ app/admin/device.rb
@@ -1,4 +1,15 @@
 ActiveAdmin.register Device do
   config.filters = false
   permit_params :id, :brand, :model, :os, :uid, :mobile_number, :app_version, :user_id, :ip, :location, :banned
+  
+  index do
+    column :user
+    column :brand
+    column :model
+    column :os
+    column :created_at
+    column :banned
+    actions
+  end
+  
 end
