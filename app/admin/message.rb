e18e9d9176bd2b2af6a61748a778651083ce0eb5
Gal Dubitski <gal@tocarta.es>
2015-03-03 13:39:53 +0000

    message admin

--- app/admin/message.rb
+++ app/admin/message.rb
@@ -6,9 +6,32 @@ ActiveAdmin.register Message do
   #
   permit_params :sender_id, :receiver_id, :ticket_id, :conversation_id, :content, :from_dingo, :visible, :read
   
-  
   preserve_default_filters!
   filter :conversation_id
+  
+  index do
+    column :sender
+    column :receiver
+    column :content
+    column :created_at
+    column :visible
+    column :read
+    
+    column "Ticket" do |message|
+      if !message.ticket.nil?
+        link_to message.ticket.name, admin_ticket_path(message.ticket_id)
+      end
+    end
+    
+    actions
+  end
+  
+  
+  
+  
+  
+  
+  
   
   form do |f|
     f.inputs "Message Inputs" do
