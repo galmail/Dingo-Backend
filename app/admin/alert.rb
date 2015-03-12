f00f43cf9e1be57242c843d1524992d96ec67884
Gal Dubitski <gal@tocarta.es>
2015-03-12 12:32:20 +0000

    alerts admin page

--- app/admin/alert.rb
+++ app/admin/alert.rb
@@ -2,6 +2,17 @@ ActiveAdmin.register Alert do
 
   permit_params :user_id, :event_id, :price, :active, :description
   
+  
+  index do
+    column :user
+    column :event
+    column :created_at
+    column :description
+    column :active
+    actions
+  end
+  
+  
   # See permitted parameters documentation:
   # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
