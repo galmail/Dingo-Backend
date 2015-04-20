2736c993f4095ff1660b79a79461ebd5a3a200b5
Gal Dubitski <gal@tocarta.es>
2015-04-20 11:13:17 +0100

    messages api

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -13,17 +13,21 @@ class Api::V1::MessagesController < Api::BaseController
       filters[:receiver_id] = params[:receiver_id]
     end
 
-    if params.has_key?(:conversations)
-      if params[:conversations]
-        query << and_query(query) << "(sender_id = ? OR receiver_id = ?)"
-        conditions << current_user.id
-        conditions << current_user.id
-      end
-    end
+    #if params.has_key?(:conversations)
+    #  if params[:conversations]
+    query << and_query(query) << "(sender_id = ? OR receiver_id = ?)"
+    conditions << current_user.id
+    conditions << current_user.id
+    #  end
+    #end
 
     if params.has_key?(:content)
       query << and_query(query) << "content ILIKE ?"
       conditions << "%#{params[:content]}%"
+    end
+    
+    if params.has_key?(:conversation_id)
+      filters[:conversation_id] = params[:conversation_id]
     end
 
     @messages = Message.where(filters).where(conditions.insert(0,query)).order('created_at DESC').limit(Settings.MESSAGES_LIMIT.to_i)
