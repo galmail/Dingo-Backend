667eb091bfdeeda8ee5bd3ccc2439fb6a6385f9f
Gal Dubitski <gal@tocarta.es>
2015-04-23 21:54:47 +0100

    get peers api

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -67,7 +67,10 @@ class Api::V1::MessagesController < Api::BaseController
     filters = { visible: true }
     query = "(sender_id = ? OR receiver_id = ?)"
     conditions = [current_user.id,current_user.id]
-    messages = Message.where(filters).where(conditions.insert(0,query)).select(:conversation_id,:sender_id,:receiver_id,:ticket_id).distinct(:conversation_id)
+    
+    messages = Message.where(filters).where(conditions.insert(0,query)).select(:conversation_id,:sender_id,:receiver_id,:ticket_id).distinct
+    messages = messages.to_a.uniq { |msg| msg.conversation_id }
+    
     messages.each { |msg|
       peer = msg.get_peer(current_user.id)
       conversations << { :id => msg.conversation_id, :user_id => peer.id, :user_name => peer.name, :user_pic => peer.photo_url, :event_name => msg.get_event_name }
